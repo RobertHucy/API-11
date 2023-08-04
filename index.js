62b95b8811ccaa41a54288368d97edd8e9f119c5
RobertHucy <133040914+RobertHucy@users.noreply.github.com>
2023-08-04 16:47:20 +0000

    .

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+import { selectUsuarios } from "./bd.js"; 
+
+dotenv.config();
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -9,17 +14,6 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota / solicitada");
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
-});
-
-//index.js
-import dotenv from "dotenv";
-
-dotenv.config();
-
-import { selectUsuarios } from "./bd.js"; 
-
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
   try {
@@ -29,3 +23,9 @@ app.get("/usuarios", async (req, res) => {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+//index.js
