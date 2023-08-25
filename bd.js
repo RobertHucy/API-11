a50242e4697f0638b9494dc5d1d3d2924e1b718f
RobertHucy <133040914+RobertHucy@users.noreply.github.com>
2023-08-25 16:40:56 +0000

    Update 02

--- bd.js
+++ bd.js
@@ -22,5 +22,26 @@ async function selectUsuario(id) {
   const res = await client.query(query, usuario);
   return res.rows;
 }
+
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+}
+
+async function deleteUsuario(id) {
+  const client = await connect();
+  const query = "DELETE FROM usuario WHERE id = $1";
+  await client.query(query, [id]);
+}
+
+async function updateUsuario(data) {
+  const client = await connect();
+  const query =
+    "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+  const usuario = [data.nome, data.email, data.senha, data.id];
+  await client.query(query, usuario);
+}
 //bd.js
-export { selectUsuarios, selectUsuario };
+export { selectUsuarios, selectUsuario, updateUsuario, deleteUsuario, insertUsuario };
