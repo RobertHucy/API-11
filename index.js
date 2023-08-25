f8bfa5839b2b2b47ddb74c76add240664c4561c5
RobertHucy <133040914+RobertHucy@users.noreply.github.com>
2023-08-25 16:37:56 +0000

    Update

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
 import express from "express";
 import dotenv from "dotenv";
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";
+
 dotenv.config();
 
-import {  selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario  } from "./bd.js";
+const app = express();
+const port = 3000;
 
-const app = express(); // Instancia o Express
-const port = 3000; // Define a porta
 app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Rota / solicitada");
-  // Cria a rota da raiz do projeto
   res.json({
-    nome: "Robert Hucy Nunes Alves", // Substitua pelo seu nome
+    nome: "Robert Hucy Nunes Alves",
   });
 });
 
@@ -24,10 +23,9 @@ app.get("/usuarios", async (req, res) => {
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
-
-  console.log("Rota GET/usuarios solicitada");
+  console.log("Rota GET /usuarios solicitada");
 });
-//index.js
+
 app.get("/usuario/:id", async (req, res) => {
   console.log("Rota GET /usuario solicitada");
   try {
@@ -38,7 +36,7 @@ app.get("/usuario/:id", async (req, res) => {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
-//index.js
+
 app.post("/usuario", async (req, res) => {
   console.log("Rota POST /usuario solicitada");
   try {
@@ -48,20 +46,20 @@ app.post("/usuario", async (req, res) => {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
-//index.js
+
 app.delete("/usuario/:id", async (req, res) => {
   console.log("Rota DELETE /usuario solicitada");
   try {
     const usuario = await selectUsuario(req.params.id);
     if (usuario.length > 0) {
       await deleteUsuario(req.params.id);
-      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
+      res.status(200).json({ message: "Usuário excluído com sucesso!" });
     } else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
-//index.js
+
 app.patch("/usuario", async (req, res) => {
   console.log("Rota PATCH /usuario solicitada");
   try {
@@ -77,6 +75,5 @@ app.patch("/usuario", async (req, res) => {
 });
 
 app.listen(port, () => {
-  // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
+  console.log(`Serviço escutando na porta: ${port}`);
 });
