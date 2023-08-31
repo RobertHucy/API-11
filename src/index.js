32cdba1f008de99c36c22160693fbaca2b7e0f2d
RobertHucy <133040914+RobertHucy@users.noreply.github.com>
2023-08-31 12:03:59 +0000

    dd

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import express from "express";
 import dotenv from "dotenv";
-import roteadorUsuario from "./routes/usuario.js";
 dotenv.config();
 
-const app = express();
-const port = 3000;
+import express from "express";   
+
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
 
 app.use(express.json());
-app.use(roteadorUsuario);
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  console.log("Rota / solicitada");
+import roteadorUsuario from "./routes/usuario.js";
+import loginRouter from "./routes/login.js";
+
+app.use(roteadorUsuario);
+app.use(loginRouter);
+
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
-    nome: "Robert Hucy Nunes Alves",
+    nome: "Robert Hucy Nunes Alves",      // Substitua pelo seu nome
   });
+  console.log("Rota / solicitada");
 });
 
-app.listen(port, () => {
-  console.log(`Serviço escutando na porta: ${port}`);
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
 });
